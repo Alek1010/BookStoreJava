92d67fb441e74b444c5d7faf140a7e0ee4295a73
Alek1010 <ali.mihaylov04@gmail.com>
2025-04-11 08:15:43 +0100

    AddedBookClass

--- server side course work/BookStoreProject/src/main/java/com/example/model/Book.java
+++ server side course work/BookStoreProject/src/main/java/com/example/model/Book.java
@@ -10,4 +10,80 @@ package com.example.model;
  */
 public class Book {
     
+    private int id;
+    private int AuthorId;
+    private String title;
+    private String isbn;
+    private int publicationYear;
+    private double price;
+    private int stock;
+
+    public Book(int id, int AuthorId, String title, String isbn, int publicationYear, double price, int stock) {
+        this.id = id;
+        this.AuthorId = AuthorId;
+        this.title = title;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAuthorId() {
+        return AuthorId;
+    }
+
+    public void setAuthorId(int AuthorId) {
+        this.AuthorId = AuthorId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
+    
+    
 }
