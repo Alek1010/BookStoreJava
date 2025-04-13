7099140f3b03d9b8d995eb70c5201913b0bd3d18
Alek1010 <ali.mihaylov04@gmail.com>
2025-04-13 19:27:52 +0100

    working on the mapper having issues

--- server side course work/BookStoreProject/src/main/java/com/example/exception/GenericExceptionMapper.java
+++ server side course work/BookStoreProject/src/main/java/com/example/exception/GenericExceptionMapper.java
@@ -8,6 +8,24 @@ package com.example.exception;
  *
  * @author alekm
  */
-public class GenericExceptionMapper {
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import java.util.Map;
+
+@Provider
+public class GenericExceptionMapper implements ExceptionMapper<RuntimeException>{
+
+    @Override
+    public Response toResponse(RuntimeException e) {
+        Response.Status status = switch (e.getClass().getSimpleName()){
+            case  "BookNotFoundException", "AuthorNotFoundException", "CustomerNotFoundException", "CartNotFoundException" -> Response.Status.NOT_FOUND;
+            case "InvalidInputException", "OutOfStockException" -> Response.Status.BAD_REQUEST;
+            default -> Response.Status.INTERNAL_SERVER_ERROR;
+        };
+        return Response.status(status).entity(Map.of("error",e.getClass().getSimpleName(),
+                "message",e.getMessage())).build();
+    }
     
 }
