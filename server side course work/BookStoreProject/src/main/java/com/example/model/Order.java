9946a988c8d83bd2e4a7774783b561b92912648a
Alek1010 <ali.mihaylov04@gmail.com>
2025-04-13 14:38:06 +0100

    Added all the needed classes

--- server side course work/BookStoreProject/src/main/java/com/example/model/Order.java
+++ server side course work/BookStoreProject/src/main/java/com/example/model/Order.java
@@ -4,10 +4,46 @@
  */
 package com.example.model;
 
+import java.util.List;
+
 /**
  *
  * @author alekm
  */
 public class Order {
+    private int orderId;
+    private int customerId;
+    private List<CartItem> items;
+
+    public Order(int orderId, int customerId, List<CartItem> items) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.items = items;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<CartItem> items) {
+        this.items = items;
+    }
+    
     
 }
